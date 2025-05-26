28e8a348509394fcf03ef2e8f50885eddd100e7e
Ujjwal Garg <garg.ujjwal04@gmail.com>
2025-05-26 17:55:19 +0530

    update transfer trans dto

--- backend/src/main/java/com/gargujjwal/military_asset_management/dto/TransferTransactionDto.java
+++ backend/src/main/java/com/gargujjwal/military_asset_management/dto/TransferTransactionDto.java
@@ -1,15 +1,14 @@
 package com.gargujjwal.military_asset_management.dto;
 
 import com.gargujjwal.military_asset_management.constants.TransferType;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TransferTransactionDto extends InventoryTransactionDto {
-  @NotNull private BaseDto sourceBase;
-  @NotNull private BaseDto destBase;
+  private BaseDto sourceBase;
+  private BaseDto destBase;
   private TransferType type;
   private String notes;
 }
